fa3a2fdb09d17fde2c29d2c937de3d4079a0647f
Feng-Kun Guo <fkguo@itp.ac.cn>
2020-07-09 14:06:24 +0800

    check

--- src/init.jl
+++ src/init.jl
@@ -2,11 +2,7 @@
 
 const minuit = PyNULL()
 const mMinuit = PyNULL()
-<<<<<<< HEAD
-const iminuit_version = "1.4.5" # this has to be >=1.4.1
-=======
-const iminuit_version = "1.4.5"
->>>>>>> abdd8c33ca57b1140ddc2b3c5d4ea73a8dbb9dc7
+const iminuit_version = "1.4.5" # this has to be at least 1.4.1
 
 # initialization -- anything that depends on Python has to go here,
 # so that it occurs at runtime (while the rest can be precompiled).
