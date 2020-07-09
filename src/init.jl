abdd8c33ca57b1140ddc2b3c5d4ea73a8dbb9dc7
Feng-Kun Guo <fkguo@itp.ac.cn>
2020-07-09 13:43:23 +0800

    set iminuit version to 1.4.5

--- src/init.jl
+++ src/init.jl
@@ -2,10 +2,11 @@
 
 const minuit = PyNULL()
 const mMinuit = PyNULL()
+const iminuit_version = "1.4.5"
 
 # initialization -- anything that depends on Python has to go here,
 # so that it occurs at runtime (while the rest can be precompiled).
 function __init__()
-    copy!(minuit, pyimport_conda("iminuit", "iminuit", "conda-forge"))
+    copy!(minuit, pyimport_conda("iminuit", "iminuit=" * iminuit_version, "conda-forge"))
     copy!(mMinuit, pyimport(:iminuit).:Minuit)
 end
